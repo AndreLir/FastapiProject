a3f7eb1fc6d77062ecbe4f849d927f79d70a9f3c
AndyLira <andreortizlira@gmail.com>
2025-05-31 23:28:48 -0500

    Primer Commit:Proyecto iniciado

---
+++ users.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Inicia el server con el comando: uvicorn users:app --reload
+
+# Entidad User
+class User(BaseModel):
+    # Atributos de la entidad User
+    id: int
+    name: str
+    surname: str
+    url: str
+    age: int
+    
+users_list = [User(id=1, name="Brais", surname="Moure",  url="https://mouredev.dev",age=35),
+         User(id=2, name="John", surname="Doe", url="https://example.com", age=30),
+         User(id=3, name="Jane", surname="Smith", url="https://example.com", age=28)]  # Corregido aquí
+
+@app.get("/usersJson")
+async def usersJson():
+    return [{"name":"Brais","surname":"Moure","url":"https://mouredev.dev","age":35},
+            {"name":"John","surname":"Doe","url":"https://example.com","age":30},
+            {"name":"Jane","surname":"Smith","url":"https://example.com","age":28}]
+            
+@app.get("/users")
+async def usersClass():
+    return users_list
+
+@app.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+
+# @app.get("/userquery/")
+# async def user(id: int):
+#     users = filter(lambda user: user.id == id, users_list)
+#     try:
+#         return list(users)[0]
+#     except IndexError:
+#         return {"error": "User not found"}, 404
+
+@app.get("/userquery/")
+async def user(id: int):
+        return search_user(id)    
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except IndexError:
+        return {"error": "User not found"}, 404
